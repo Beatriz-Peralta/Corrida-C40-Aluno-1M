85ad2624ca08f549f63738999ec1e6e66730b682
Beatriz-Peralta <114191631+Beatriz-Peralta@users.noreply.github.com>
2023-03-28 18:36:54 -0300

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -32,16 +32,28 @@ class Game {
 
     // C40
     //criar novo grupo para fuels e powerCoins
-
+    fuels = new Group();
+    powerCoins = new Group();
     // Adicione o sprite de combustível ao jogo
-
+    this.addSprites(fuels,4, fuelImage,0.02);
+    
     // Adicione o sprite de moeda ao jogo
+    this.addSprites(powerCoins, 18, poweerCoinImage,0.09);
   }
 
   // C40
   addSprites(spriteGroup, numberOfSprites, spriteImage, scale) {
     for (var i = 0; i < numberOfSprites; i++) {
-     
+      var x,y;
+      
+       x = randow (width / 2 + 150,width / 2 -150); 
+      y = randow (-height * 4.5,height - 400);
+      
+      var sprite = createSprite(x,y);
+      sprite.addImage("sprite", spriteImage);
+      
+      sprite.scale = scale;
+      spriteGroup.add(sprite);
     }
   }
 
@@ -74,8 +86,12 @@ class Game {
 
         // C40  Marcar o jogador atual com o ellipse/circulo vermelho
         if (index === player.index) {
-          
+           stroke(10);
+           fill("red");
+          ellipse(x,y,60, 60);
 
+          this.handleFuel(index);
+          this.handlePowerCoins(index);
         }
       }
 
@@ -91,7 +107,12 @@ class Game {
 
   handleFuel(index) {
     // C40 Adicione o combustível como foi feito com as moedas (PowerCoins)
-   
+    cars[index - 1].overlap(powerCoins, function(collector, collected) {
+      player.fuel = 185;
+      //collected (coletado) é o sprite no grupo de colecionáveis que desencadeia
+      //o evento
+      collected.remove();
+    });
   }
 
   handlePowerCoins(index) {
